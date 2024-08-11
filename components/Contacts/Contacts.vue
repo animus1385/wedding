544366d6cace32852d66015101d33803cb98d615
Animus1385 <raspopov-re@ranepa.ru>
2024-08-11 11:21:05 +0300

    add links

--- components/Contacts/Contacts.vue
+++ components/Contacts/Contacts.vue
@@ -3,8 +3,17 @@
         <div class="contacts__container container">
             <h2 class="contacts__title title-level-2" data-aos="fade" data-aos-delay="500">КОНтакты</h2>
             <div class="contacts__names">
-                <span class="contacts__man text-1" data-aos="fade-right" data-aos-delay="700">Жених :</span>
-                <span class="contacts__woman text-1" data-aos="fade-right" data-aos-delay="900">Невеста :</span>
+                <a href="https://t.me/Nazarov_Bek" target="_blank" class="contacts__man text-1" data-aos="fade-right" data-aos-delay="700"
+                    >Жених : https://t.me/Nazarov_Bek</a
+                >
+                <a
+                    href="https://t.me/sorakhon1tosh"
+                    target="_blank"
+                    class="contacts__woman text-1"
+                    data-aos="fade-right"
+                    data-aos-delay="900"
+                    >Невеста : https://t.me/sorakhon1tosh</a
+                >
             </div>
         </div>
     </section>
