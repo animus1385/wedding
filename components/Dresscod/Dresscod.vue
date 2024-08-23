803544ba243ba9a2fd5f690dd50c40030cbfa429
Animus1385 <raspopov-re@ranepa.ru>
2024-08-23 20:23:29 +0300

    fix select

--- components/Dresscod/Dresscod.vue
+++ components/Dresscod/Dresscod.vue
@@ -2,11 +2,6 @@
     <section class="dresscod">
         <div class="dresscod__container container">
             <h2 class="dresscod__title title-level-2" data-aos="fade" data-aos-delay="500" data-aos-once="true">Дресс - код</h2>
-            <p class="dresscod__descr text-1" data-aos="fade" data-aos-delay="700" data-aos-once="true">
-                Будем благодарны, если при выборе нарядов на наше торжество вы исключите полность белые образы.
-            </p>
-            <br />
-            <br />
             <p class="dresscod__descr text-1" data-aos="fade" data-aos-delay="900" data-aos-once="true">
                 Дамам: Будем благодарны, если при выборе нарядов на наше торжество, Вы исключите полностью белые образы.
             </p>
